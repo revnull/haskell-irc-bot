d5f5007d2b9900c4dca267b5ec6ec0f8a281e751
Rev. Johnny Healey <john.healey@cmgdigital.com>
2010-08-29 08:59:26 -0400

    Working on the implementation of the BoggleBot

--- IRC/Bots/Boggle.hs
+++ IRC/Bots/Boggle.hs
@@ -21,84 +21,44 @@ import Data.Array
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 import Random
-import Maybe
+import IRC.Bots.Boggle.Board
 
-type Board = Array (Int,Int) String
 type Scores = Map.Map String Int
 type WordSet = Set.Set String
 
 data Game = Game Board Integer Scores WordSet
-data BoggleBot = BoggleBot (Maybe Game) Random.StdGen Int
+data BoggleBot = BoggleBot (Maybe Game) StdGen Int
 
-letterFreqs :: Map.Map String [Int]
-letterFreqs = Map.fromList [
-    ("Qu", [3]),
-    ("A", [73, 15, 2, 1, 1]),
-    ("C", [43, 6, 1]),
-    ("B", [21, 2, 1, 1]),
-    ("E", [100, 35, 7, 1, 1]),
-    ("D", [43, 6, 1, 1]),
-    ("G", [35, 4, 1, 1]),
-    ("F", [16, 2, 1, 1]),
-    ("I", [81, 22, 4, 1, 1, 1]),
-    ("H", [26, 2, 1]),
-    ("K", [11, 1, 1, 1]),
-    ("J", [3, 1]),
-    ("M", [29, 4, 1, 1]),
-    ("L", [53, 10, 1, 1]),
-    ("O", [58, 13, 2, 1]),
-    ("N", [69, 16, 2, 1, 1]),
-    ("P", [32, 4, 1, 1]),
-    ("S", [82, 23, 4, 1, 1, 1]),
-    ("R", [73, 14, 1, 1]),
-    ("U", [36, 3, 1, 1]),
-    ("T", [68, 15, 2, 1]),
-    ("W", [11, 1, 1]),
-    ("V", [13, 1]),
-    ("Y", [18, 1]),
-    ("X", [4, 1]),
-    ("Z", [5, 1, 1, 1])
-    ]
+getGame = do
+    (BoggleBot game _ _) <- get
+    return game
 
-getLetter i ((l,(x:_)):xs) =
-    if x >= i
-        then l
-        else getLetter (i - x) xs
+putGame game = do
+    (BoggleBot _ gen sb) <- get
+    put (BoggleBot game gen sb)
 
-updateFreqs l total freqs =
-    let (x:xs) = fromJust $ Map.lookup l freqs
-        freqs' = if xs == []
-            then Map.delete l freqs
-            else Map.insert l xs freqs
-    in (total - x, freqs')
+getGen = do
+    (BoggleBot _ gen _) <- get
+    return gen
 
-letterIter gen _ _ 0 = ([],gen)
-letterIter gen total freqs remaining =
-    let (i, gen') = randomR (0,total - 1) gen
-        val = getLetter i $ Map.toList freqs
-        (total', freqs') = updateFreqs val total freqs
-        (next, gen'') = letterIter gen' total' freqs' (remaining - 1)
-    in (val:next, gen'')
+putGen gen = do
+    (BoggleBot game _ sb) <- get
+    put (BoggleBot game gen sb)
 
-randomLetters gen count =
-    let totalCount = sum $ [head l | (_,l) <- Map.toList letterFreqs]
-    in letterIter gen totalCount letterFreqs count
+getScrollback = do
+    (BoggleBot _ _ sb) <- get
+    return sb
+
+putScrollback sb = do
+    (BoggleBot game gen _) <- get
+    put (BoggleBot game gen sb)
 
 makeRandomBoard :: OutputEvent BoggleBot Board
 makeRandomBoard = do
-    (BoggleBot game gen sb) <- get
-    let (vals, gen') = randomLetters gen 16
-    put (BoggleBot game gen' sb)
-    return $ array ((0,0),(3,3)) $ do
-        (i,l) <- zip [0..] vals
-        return (divMod i 4, l) 
-
-rows :: Board -> [[String]]
-rows board = do
-    row <- [0..3]
-    return $ do
-        col <- [0..3]
-        return $ board!(row,col)
+    gen <- getGen
+    let (board, gen') = makeBoard gen
+    putGen gen'
+    return board
 
 formatLetter l = 
     if length l == 1
@@ -112,6 +72,11 @@ outputBoard b = do
 
 initialBoggle init = BoggleBot Nothing (mkStdGen init) 0
 
+finishGame = do
+    (BoggleBot game gen _) <- get
+    privMsg "Time is up!"
+    put (BoggleBot Nothing gen 0)
+
 play :: Bot BoggleBot
 play msg ts = undefined
 
@@ -122,6 +87,7 @@ startGame msg ts =
         Just _ -> do
             board <- makeRandomBoard
             outputBoard board
+            delayEvent (ts + 180) finishGame
         Nothing -> return ()
 
 boggle :: Bot BoggleBot
---
+++ IRC/Bots/Boggle/Board.hs
@@ -0,0 +1,91 @@
+-- Copyright 2010 Rev. Johnny Healey
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+module IRC.Bots.Boggle.Board (Board, makeBoard, rows) where
+
+import Random
+import qualified Data.Map as Map
+import Data.Array
+import Maybe
+
+type Board = Array (Int,Int) String
+
+letterFreqs :: Map.Map String [Int]
+letterFreqs = Map.fromList [
+    ("Qu", [3]),
+    ("A", [73, 15, 2, 1, 1]),
+    ("C", [43, 6, 1]),
+    ("B", [21, 2, 1, 1]),
+    ("E", [100, 35, 7, 1, 1]),
+    ("D", [43, 6, 1, 1]),
+    ("G", [35, 4, 1, 1]),
+    ("F", [16, 2, 1, 1]),
+    ("I", [81, 22, 4, 1, 1, 1]),
+    ("H", [26, 2, 1]),
+    ("K", [11, 1, 1, 1]),
+    ("J", [3, 1]),
+    ("M", [29, 4, 1, 1]),
+    ("L", [53, 10, 1, 1]),
+    ("O", [58, 13, 2, 1]),
+    ("N", [69, 16, 2, 1, 1]),
+    ("P", [32, 4, 1, 1]),
+    ("S", [82, 23, 4, 1, 1, 1]),
+    ("R", [73, 14, 1, 1]),
+    ("U", [36, 3, 1, 1]),
+    ("T", [68, 15, 2, 1]),
+    ("W", [11, 1, 1]),
+    ("V", [13, 1]),
+    ("Y", [18, 1]),
+    ("X", [4, 1]),
+    ("Z", [5, 1, 1, 1])
+    ]
+
+getLetter i ((l,(x:_)):xs) =
+    if x >= i
+        then l
+        else getLetter (i - x) xs
+
+updateFreqs l total freqs =
+    let (x:xs) = fromJust $ Map.lookup l freqs
+        freqs' = if xs == []
+            then Map.delete l freqs
+            else Map.insert l xs freqs
+    in (total - x, freqs')
+
+letterIter gen _ _ 0 = ([],gen)
+letterIter gen total freqs remaining =
+    let (i, gen') = randomR (0,total - 1) gen
+        val = getLetter i $ Map.toList freqs
+        (total', freqs') = updateFreqs val total freqs
+        (next, gen'') = letterIter gen' total' freqs' (remaining - 1)
+    in (val:next, gen'')
+
+randomLetters gen count =
+    let totalCount = sum $ [head l | (_,l) <- Map.toList letterFreqs]
+    in letterIter gen totalCount letterFreqs count
+
+makeBoard gen =
+    let (vals, gen') = randomLetters gen 16
+        board = array ((0,0),(3,3)) $ do
+            (i,l) <- zip [0..] vals
+            return (divMod i 4, l) 
+    in (board, gen')
+
+rows :: Board -> [[String]]
+rows board = do
+    row <- [0..3]
+    return $ do
+        col <- [0..3]
+        return $ board!(row,col)
+
